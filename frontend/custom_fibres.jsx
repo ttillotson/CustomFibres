25aa7c9100101f107e8ced68da57f6d7c690f819
Travis Tillotson <travis.don.tillotson@gmail.com>
2018-06-04 23:30:17 -0700

    Config Entry File

---
+++ frontend/custom_fibres.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configStore from './store/store';
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    let store;
+    if (window.currentUser) {
+        const preLoadedState = { session: {currentUser: window.currentUser}};
+        store = configStore(preLoadedState);
+        delete window.currentUser;
+    } else {
+        store = configStore();
+    }
+
+    ReactDOM.render(<Root store={store} />, root);
+});
