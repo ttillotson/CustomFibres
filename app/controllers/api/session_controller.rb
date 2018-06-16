87cdd2177b551c9572f37f55b0931f4a03b71d9e
Travis Tillotson <travis.don.tillotson@gmail.com>
2018-06-16 13:20:38 -0700

    Set up Session Controller

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,2 +1,28 @@
 class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(user_params)
+
+        if @user 
+            sign_in(@user)
+            render
+        else 
+            render json: ['Invalid Credentials'], status: 422
+        end
+    end
+
+    def destroy 
+        @user = current_user 
+
+        if @user 
+            sign_out
+        else
+            render json: ['Nobody is currently signed in.'], status: 404
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
