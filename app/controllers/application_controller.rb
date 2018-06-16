96a7daccb03be5ed3757de80b820d332a9976668
Travis Tillotson <travis.don.tillotson@gmail.com>
2018-06-16 14:21:02 -0700

    Setup Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :signed_in?
+
+  private 
+
+  def current_user 
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def sign_in(user)
+    session[:session_token] = user.reset_token!
+  end
+
+  def sign_out
+    current_user.reset_token!
+    session[:session_token] = nil
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def require_log_in
+    render json: ['Require Login'], status: 422 unless signed_in?
+  end
 end
