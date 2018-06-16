22f2a6f93c238abbcf8cbd4c55d59125c5fdbdf6
Travis Tillotson <travis.don.tillotson@gmail.com>
2018-06-16 13:01:58 -0700

    Set Up PageInfo Controller

--- app/controllers/api/page_info_controller.rb
+++ app/controllers/api/page_info_controller.rb
@@ -1,23 +1,39 @@
 class Api::PageInfoController < ApplicationController
     def index
-
+        @info = PageInfo.all
     end
 
     def create
+        @info = PageInfo.new(info_params)
 
+        if @info.save 
+            render :show
+        else 
+            render json: @info.errors.full_messages, status: 422
+        end
     end
 
     def update
+        @info = PageInfo.find_by(params[:id])
 
+        if @info.update_attributes(info_params)
+            render :show
+        else
+            render json: @info.errors.full_messages, status: 422
+        end
     end
 
     def destroy
+        @info = PageInfo.find_by(params[:id])
 
+        unless @info.destroy 
+            render json: @info.errors.full_messages, status: 422
+        end
     end
 
     private
 
     def info_params
-        # params.require(:info).permit()
+        params.require(:info).permit(:title, :body)
     end
 end
--- test/models/page_info_test.rb
+++ test/models/page_info_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: page_infos
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string
+#  body       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PageInfoTest < ActiveSupport::TestCase
